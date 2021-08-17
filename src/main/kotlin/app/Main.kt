0918f198bd9310ebb2de1d3a18fe3a3278e6029a
nobreconfrade <willpereirabr@gmail.com>
2021-08-16 22:42:29 -0300

    Routes for user manipulation

--- src/main/kotlin/app/Main.kt
+++ src/main/kotlin/app/Main.kt
@@ -1,7 +1,42 @@
 package app
+import app.user.User
+import app.user.UserDao
 import io.javalin.Javalin;
 
 fun main(args: Array<String>){
-    val app = Javalin.create().start(7000)
-    app.get("/") { ctx -> ctx.result("Hello World!") }
+    val userDao = UserDao()
+    val app = Javalin.create().apply {
+        exception(Exception::class.java) {e, ctx -> e.printStackTrace()}
+        error(404) {ctx -> ctx.json("not found")}
+    }.start(7000)
+
+    app.get("/users") { ctx ->
+        ctx.json(userDao.users)
+    }
+    app.get("/users/:user-id") { ctx ->
+        ctx.json(userDao.findById(ctx.pathParam("user-id").toInt())!!)
+    }
+    app.get("/users/email/:email") { ctx ->
+        ctx.json(userDao.findByEmail(ctx.pathParam("email"))!!)
+    }
+
+    app.post("/users") { ctx ->
+        val user = ctx.body<User>()
+        userDao.save(name = user.name, email = user.email)
+        ctx.status(201)
+    }
+
+    app.patch("/users/:user-id") { ctx ->
+        val user = ctx.body<User>()
+        userDao.update(
+            id = ctx.pathParam("user-id").toInt(),
+            user = user
+        )
+        ctx.status(204)
+    }
+
+    app.delete("/users/:user-id") { ctx ->
+        userDao.delete(ctx.pathParam("user-id").toInt())
+        ctx.status(204)
+    }
 }
