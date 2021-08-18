87f080b8ad1320241670883d6b1e768fefa6a3e3
nobreconfrade <willpereirabr@gmail.com>
2021-08-18 14:57:38 -0300

    Parse response data

--- src/main/kotlin/app/controller/ExchangerController.kt
+++ src/main/kotlin/app/controller/ExchangerController.kt
@@ -13,6 +13,13 @@ import java.util.logging.Logger
 class ExchangerController {
     val logger = Logger.getLogger("ExchangerController")
 
+    var placeholder_db = hashMapOf<String, Float>(
+        "USD" to 0.0f,
+        "BRL" to 0.0f,
+        "EUR" to 0.0f,
+        "JPY" to 0.0f
+    )
+
     fun sendExchangeRequest(){
         var params = "app_id=" + URLEncoder.encode(API_KEY, "UTF-8")
         params += "&base=USD"
@@ -34,10 +41,21 @@ class ExchangerController {
 
                 var inputLine = it.readLine()
                 while (inputLine != null) {
-                    response.append(inputLine)
+                    var parts = inputLine.split(":")
+                    if (parts.size == 2) {
+                        // Clean the id string, remove whitespaces and double-quotes for identification
+                        var id = parts[0].trim().replace("\"","")
+                        // Clean the value string, remove whitespaces and trailing comma them transform to float
+                        var rate = parts[1].trim().trim(',')
+                        if (arrayOf("BRL","JPY","EUR").contains(id)){
+                            placeholder_db[id] = rate.toFloat()
+                        }
+                    }
+//                    response.append(inputLine)
                     inputLine = it.readLine()
                 }
                 logger.info("Response : $response")
+                logger.info("Parsed value : $placeholder_db")
             }
         }
     }
