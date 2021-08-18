d16d4986fda0d439a06714ab7de053f0c7e0c7f0
nobreconfrade <willpereirabr@gmail.com>
2021-08-18 00:50:55 -0300

    Exchanger coroutines for currency and logging

--- src/main/kotlin/app/Main.kt
+++ src/main/kotlin/app/Main.kt
@@ -1,42 +1,63 @@
 package app
-import app.model.User
-import app.controller.UserDao
-import io.javalin.Javalin;
+import app.controller.ExchangerController
+import app.controller.UserController
+import app.scheduler.ExchangerScheduler
+import io.javalin.Javalin
+import java.util.logging.Logger
+
+
+data class UserInput(val id: String,
+                     val from: String,
+                     val to: String,
+                     val value: Float)
+
+val logger = Logger.getLogger("root")
+
 
 fun main(args: Array<String>){
-    val userDao = UserDao()
     val app = Javalin.create().apply {
         exception(Exception::class.java) {e, ctx -> e.printStackTrace()}
         error(404) {ctx -> ctx.json("not found")}
     }.start(7000)
 
-    app.get("/users") { ctx ->
-        ctx.json(userDao.users)
-    }
-    app.get("/users/:user-id") { ctx ->
-        ctx.json(userDao.findById(ctx.pathParam("user-id").toInt())!!)
-    }
-    app.get("/users/email/:email") { ctx ->
-        ctx.json(userDao.findByEmail(ctx.pathParam("email"))!!)
-    }
+    val exchanger = ExchangerController()
+    val exchangerTask = ExchangerScheduler(exchanger, COROUTINE_INTERVAL)
+    exchangerTask.startRoutine()
 
-    app.post("/users") { ctx ->
-        val user = ctx.body<User>()
-        userDao.save(name = user.name, email = user.email)
+    logger.info("Setting up routes")
+    app.post("/transaction"){ ctx ->
+        var data = ctx.body<UserInput>()
         ctx.status(201)
     }
+    logger.info("Routes ready")
 
-    app.patch("/users/:user-id") { ctx ->
-        val user = ctx.body<User>()
-        userDao.update(
-            id = ctx.pathParam("user-id").toInt(),
-            user = user
-        )
-        ctx.status(204)
-    }
-
-    app.delete("/users/:user-id") { ctx ->
-        userDao.delete(ctx.pathParam("user-id").toInt())
-        ctx.status(204)
-    }
+//    app.get("/users") { ctx ->
+//        ctx.json(userController.users)
+//    }
+//    app.get("/users/:user-id") { ctx ->
+//        ctx.json(userController.findById(ctx.pathParam("user-id").toInt())!!)
+//    }
+//    app.get("/users/email/:email") { ctx ->
+//        ctx.json(userController.findByEmail(ctx.pathParam("email"))!!)
+//    }
+//
+//    app.post("/users") { ctx ->
+//        val user = ctx.body<User>()
+//        userController.save(name = user.name, email = user.email)
+//        ctx.status(201)
+//    }
+//
+//    app.patch("/users/:user-id") { ctx ->
+//        val user = ctx.body<User>()
+//        userController.update(
+//            id = ctx.pathParam("user-id").toInt(),
+//            user = user
+//        )
+//        ctx.status(204)
+//    }
+//
+//    app.delete("/users/:user-id") { ctx ->
+//        userController.delete(ctx.pathParam("user-id").toInt())
+//        ctx.status(204)
+//    }
 }
