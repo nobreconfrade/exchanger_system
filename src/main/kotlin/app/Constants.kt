453d65e9a0bd98cd2bffe4d9d53e2a7f464fe583
nobreconfrade <willpereirabr@gmail.com>
2021-08-18 00:52:16 -0300

    Constants file

---
+++ src/main/kotlin/app/Constants.kt
@@ -0,0 +1,6 @@
+package app
+
+
+val API_KEY = "9d60c5ee1869482095954112cda09e73" // Open Exchange Rate app id
+val API_URL = "https://openexchangerates.org/api/latest.json?" // Open Exchange Rate URL
+val COROUTINE_INTERVAL : Long = 600 * 1000 // In milliseconds
